45043c86562f03aaa6f90823905e30173188d074
Lawrence <mlawrenceignacio@gmail.com>
2025-07-10 20:36:49 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+
 import todoRoutes from "./routes/todo.route.js";
 import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 
 app.use("/api/todos", todoRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+}
 
 connectDB()
   .then(() => {
